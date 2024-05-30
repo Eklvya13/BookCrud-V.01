9bfb405cf61b6bd6bfc06b97ee7dabcbd4102b2b
Eklvya13 <hariacharya7881@gmail.com>
2024-05-30 22:40:33 +0530

    going to implement data manipulation on restful routes

---
+++ data/books.js
@@ -0,0 +1,68 @@
+const { readFileSync, writeFileSync } = require('fs')
+const path = require('path')
+
+const bookJsonPath = path.join(__dirname, './books.json')
+const data = readFileSync(bookJsonPath, 'utf8');
+
+
+let booksJson = JSON.parse(data)
+
+const writeData = (updatedData) => {
+    writeFileSync(bookJsonPath, JSON.stringify(updatedData, null, 2), 'utf-8');
+};
+
+const searchBooks = (key) => {
+    const regex = new RegExp(`${key}`, 'i')
+    const queryBooks = booksJson.filter((book) => regex.test(book.name))
+    return queryBooks;
+}
+
+const insertBook = (newBookObject) => {
+    try {       
+        const lastId = booksJson[booksJson.length - 1].id
+        newBookObject.id = lastId + 1
+        booksJson.push(newBookObject);
+        writeData(booksJson);
+        return true;
+    } catch (error) {
+        console.log(error);
+        return false
+    }
+}
+
+const deleteBook = (id) => {
+    try {       
+        const index = booksJson.findIndex((book) => book.id === id)
+        if (index == -1){
+            return false;
+        }
+        booksJson.splice(index, 1);
+        writeData(booksJson);
+        return true;
+    } catch (error) {
+        console.log(err);
+        return false;
+    }
+}
+
+const updateBook = (id, newBookObject) => {
+    try {       
+        const index = booksJson.findIndex((book) => book.id === id)
+        if (index == -1){
+            return false;
+        }
+        booksJson[index] = { ...booksJson[index], ...newBookObject}
+        writeData(booksJson);
+        return true;
+    } catch (error) {
+        console.log(err);
+        return false;
+    }
+}
+
+module.exports = {
+    updateBook,
+    deleteBook,
+    insertBook,
+    searchBooks
+}
